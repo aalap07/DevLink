c024c36ef6ef7c19a64abb8ce9d4b9b4b030a8c1
Aalap Patel <aalappatel07@gmail.com>
2020-02-19 19:25:48 -0600

    restructure into package

---
+++ devlink/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x86,\xc8\x02\x1c\x86\x06(\x15\xa7\xb9\x81\xda5\xd3I\\\x01]\xc8b\xea\x83-'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///records.db'
+db = SQLAlchemy(app)
+
+from devlink import routes
