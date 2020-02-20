ff4863b0a90cc3614ccd80e9fad31377d5be0e8e
Aalap Patel <aalappatel07@gmail.com>
2020-02-19 22:31:01 -0600

    login functional, conditional rendering of login/logout

--- devlink/__init__.py
+++ devlink/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\x86,\xc8\x02\x1c\x86\x06(\x15\xa7\xb9\x81\xda5\xd3I\\\x01]\xc8b\xea\x83-'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///records.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from devlink import routes
