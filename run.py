8fb47fdd59186f07824bf154853441e86d22c5fb
Aalap Patel <aalappatel07@gmail.com>
2020-05-11 19:19:02 +0000

    prod version, update run script to match sg inboundrules

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from devlink import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+    host=os.getenv('LISTEN', '0.0.0.0'),
+    port=int(os.getenv('PORT', '80'))
+)
